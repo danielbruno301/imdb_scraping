820336dee86799293084061904ba441c746d1f7a
Daniel <danielbruno301@gmail.com>
2018-02-15 23:57:52 -0300

    First commit in web scraping study

---
+++ christmas_movies.py
@@ -0,0 +1,85 @@
+import requests # to connect to web pages
+from bs4 import BeautifulSoup # to extract information from html 
+import pandas as pd # to manipulate data and export to csv format
+
+def get_synopsis(soup):
+	synopsis = soup.find('div', attrs={'class':'summary_text'})
+	if synopsis.text:
+		return synopsis.text
+	else:
+		return None
+
+def get_director(soup):
+	director =  soup.find('span', attrs={'class':'itemprop'}, itemprop='name') 
+	if director.text:
+		return director.text
+	else:
+		return None
+
+def get_genre(soup):
+	genre = soup.find('div', attrs={'class':'see-more inline canwrap'}, itemprop='genre')
+	if genre != None:
+		return [a.text for a in genre.findAll('a')]
+	else:
+		return None
+
+def get_country(soup):
+	country_tags = soup.select("a[href*=country_of_origin]")
+	if len(country_tags) > 0:
+		return [c.text for c in country_tags]
+	else:
+		return None
+
+def get_idiom(soup):
+	language_tags = soup.select("a[href*=primary_language]")
+	if len(language_tags) > 0:
+		return language_tags[0].text
+	else:
+		return None
+		
+def get_duration(soup):
+	pass
+
+def get_score(soup):
+	pass
+
+def get_title(soup):
+	pass
+
+def get_production_year(soup):
+	pass
+
+def get_qtd_review(soup):
+	pass
+
+url_fonte = "http://www.imdb.com/find?q=Christmas&s=tt&ref_=fn_tt"
+# url_movie = "http://www.imdb.com/title/tt1260564/?ref_=fn_tt_tt_200"
+url_movie = "http://www.imdb.com/title/tt1410207/?ref_=fn_tt_tt_1"
+
+print(url_movie)
+con = requests.get(url_movie)
+print(con.status_code)
+
+# create BeautifulSoup object with the html page
+soup = BeautifulSoup(con.content, "html.parser")
+# print(soup.prettify())
+
+# titulos_filmes = [tag.find('a').contents[0] for tag in soup.findAll('td', attrs={'class':'result_text'})]
+# print(titulos_filmes)
+
+# movies_links = ['http://www.imdb.com' + tag.a['href']  for tag in soup.findAll('td', attrs={'class':'result_text'})]
+# print(movies_links)
+synopsis = get_synopsis(soup)
+print(synopsis)
+
+director = get_director(soup)
+print(director)
+
+genre = get_genre(soup)
+print(genre)
+
+country = get_country(soup)
+print(country)
+
+idiom = get_idiom(soup)
+print(idiom)
