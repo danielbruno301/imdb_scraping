91895013a1c2fb86660e2d68bf8c408b0394132d
Daniel <danielbruno301@gmail.com>
2018-06-08 00:14:41 -0300

    Adequate code to PEP8

--- christmas_movies.py
+++ christmas_movies.py
@@ -1,56 +1,79 @@
-import requests # to connect to web pages
-from bs4 import BeautifulSoup # to extract information from html 
-import pandas as pd # to manipulate data and export to csv format
+"""File that scrap IMDB website."""
+
+import requests  # to connect to web pages
+from bs4 import BeautifulSoup  # to extract information from html
+import pandas as pd  # to manipulate data and export to csv format
+
 
 def get_synopsis(soup):
-	synopsis = soup.find('div', attrs={'class':'summary_text'})
-	if synopsis.text:
-		return synopsis.text
-	else:
-		return None
+    """Get synopsis."""
+    synopsis = soup.find('div', attrs={'class': 'summary_text'})
+    if synopsis.text:
+        return synopsis.text
+    else:
+        return None
+
 
 def get_director(soup):
-	director =  soup.find('span', attrs={'class':'itemprop'}, itemprop='name') 
-	if director.text:
-		return director.text
-	else:
-		return None
+    """Get director info."""
+    director = soup.find('span', attrs={'class': 'itemprop'}, itemprop='name')
+    if director.text:
+        return director.text
+    else:
+        return None
+
 
 def get_genre(soup):
-	genre = soup.find('div', attrs={'class':'see-more inline canwrap'}, itemprop='genre')
-	if genre != None:
-		return [a.text for a in genre.findAll('a')]
-	else:
-		return None
+    """Get genre info."""
+    genre = soup.find('div', attrs={'class': 'see-more inline canwrap'}, itemprop='genre')
+    if genre is not None:
+        return [a.text for a in genre.findAll('a')]
+    else:
+        return None
+
 
 def get_country(soup):
-	country_tags = soup.select("a[href*=country_of_origin]")
-	if len(country_tags) > 0:
-		return [c.text for c in country_tags]
-	else:
-		return None
+    """Get country info."""
+    country_tags = soup.select("a[href*=country_of_origin]")
+    if len(country_tags) > 0:
+        return [c.text for c in country_tags]
+    else:
+        return None
+
 
 def get_idiom(soup):
-	language_tags = soup.select("a[href*=primary_language]")
-	if len(language_tags) > 0:
-		return language_tags[0].text
-	else:
-		return None
-		
+    """Get idiom info."""
+    language_tags = soup.select("a[href*=primary_language]")
+    if len(language_tags) > 0:
+        return language_tags[0].text
+    else:
+        return None
+
+
 def get_duration(soup):
-	pass
+    """Get duration info."""
+    pass
+
 
 def get_score(soup):
-	pass
+    """Get movie score info."""
+    pass
+
 
 def get_title(soup):
-	pass
+    """Get movie title."""
+    pass
+
 
 def get_production_year(soup):
-	pass
+    """Get production movie year."""
+    pass
+
 
 def get_qtd_review(soup):
-	pass
+    """Get quantity of reviews."""
+    pass
+
 
 url_fonte = "http://www.imdb.com/find?q=Christmas&s=tt&ref_=fn_tt"
 # url_movie = "http://www.imdb.com/title/tt1260564/?ref_=fn_tt_tt_200"
